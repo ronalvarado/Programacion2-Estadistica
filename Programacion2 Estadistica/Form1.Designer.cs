11adc1e669f801fe532e163347aeb3033a78a9bb
JulissaOrellana <orellanaoseguedajulissalisbeth@gmail.com>
2020-02-12 13:41:55 -0600

    estadistica

---
+++ Programacion2 Estadistica/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Programacion2_Estadistica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblserie = new System.Windows.Forms.Label();
+            this.txtserie = new System.Windows.Forms.TextBox();
+            this.lblrespuesta = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblserie
+            // 
+            this.lblserie.AutoSize = true;
+            this.lblserie.Location = new System.Drawing.Point(41, 62);
+            this.lblserie.Name = "lblserie";
+            this.lblserie.Size = new System.Drawing.Size(31, 13);
+            this.lblserie.TabIndex = 0;
+            this.lblserie.Text = "Serie";
+            // 
+            // txtserie
+            // 
+            this.txtserie.Location = new System.Drawing.Point(151, 55);
+            this.txtserie.Name = "txtserie";
+            this.txtserie.Size = new System.Drawing.Size(100, 20);
+            this.txtserie.TabIndex = 1;
+            // 
+            // lblrespuesta
+            // 
+            this.lblrespuesta.AutoSize = true;
+            this.lblrespuesta.Location = new System.Drawing.Point(33, 120);
+            this.lblrespuesta.Name = "lblrespuesta";
+            this.lblrespuesta.Size = new System.Drawing.Size(14, 13);
+            this.lblrespuesta.TabIndex = 2;
+            this.lblrespuesta.Text = "X";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 173);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Calcular X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblrespuesta);
+            this.Controls.Add(this.txtserie);
+            this.Controls.Add(this.lblserie);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblserie;
+        private System.Windows.Forms.TextBox txtserie;
+        private System.Windows.Forms.Label lblrespuesta;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Programacion2 Estadistica/estadistica.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programacion2_Estadistica
+{
+    class estadistica
+    {
+        public double suma = 0;
+        private string valor;
+
+        public double media(String[] serie)
+        {
+            foreach (string valor in serie)
+            {
+                suma += int.Parse(valor);
+
+            }
+            return suma / serie.Length;
+        }
+    }
+}
